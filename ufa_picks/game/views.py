c3cd615540d5d8d846ce9936ece1a47252af8b2d
Ryan Scott <ryan.t.scott73@gmail.com>
2026-02-20 11:46:38 -0700

    Update routes to take a year

--- ufa_picks/game/views.py
+++ ufa_picks/game/views.py
@@ -13,19 +13,24 @@ from ufa_picks.user.models import Pick
 blueprint = Blueprint("game", __name__, url_prefix="/games", static_folder="../static")
 
 
-# @login_required
-@blueprint.route("/")
+@blueprint.route("/", defaults={"year": None})
+@blueprint.route("/<string:year>/")
 @login_required
-def main():
+def main(year):
     """List weeks."""
-    weeks = Game.query.with_entities(Game.week).distinct().order_by(Game.week).all()
+    weeks = Game.query\
+        .filter_by(season=year if year else str(dt.datetime.now().year))\
+        .with_entities(Game.week)\
+        .distinct()\
+        .order_by(Game.week)\
+        .all()
     weeks = [w[0] for w in weeks][:13]
-    return render_template("games/games.html", weeks=weeks)
+    return render_template("games/games.html", weeks=weeks, year=year)
 
 
-def pre_lock(week_num):
+def pre_lock(year, week_num):
     token_form = GamePick(prefix='token')
-    week_games = Game.query.filter_by(week=week_num).order_by(Game.start_timestamp).all()
+    week_games = Game.query.filter_by(season=year, week=week_num).order_by(Game.start_timestamp).all()
     games_with_forms = []
     for g in week_games:
         game_form = GamePick(prefix=f'game_{g.id}')
@@ -56,24 +61,32 @@ def pre_lock(week_num):
                 updated_picks.append(pick)
         db.session.commit()
         flash('Your picks have been saved!', 'success')
-        return redirect(url_for('.week', week_num=week_num))
+        return redirect(url_for('.week', week_num=week_num, year=year))
 
-    return render_template('games/pre_week.html', games=games_with_forms, week_num=week_num, form=token_form)
-
-
-def post_lock(week_num):
-    week_games = Game.query.filter_by(week=week_num).order_by(Game.start_timestamp).all()
-    return render_template('games/post_week.html', games=week_games, week_num=week_num)
+    return render_template('games/pre_week.html', games=games_with_forms, week_num=week_num, year=year, form=token_form)
 
 
-@blueprint.route('/week-<int:week_num>', methods=['GET', 'POST'])
+def post_lock(year, week_num):
+    week_games = Game.query.filter_by(season=year, week=week_num).order_by(Game.start_timestamp).all()
+    return render_template('games/post_week.html', games=week_games, week_num=week_num, year=year)
+
+
+@blueprint.route('/week-<int:week_num>', methods=['GET', 'POST'], defaults={'year': None})
+@blueprint.route('/<string:year>/week-<int:week_num>', methods=['GET', 'POST'])
 @login_required
-def week(week_num):
+def week(week_num, year):
+    if year is None:
+        year = str(dt.datetime.now().year)
+
     if week_num > 13:
-        return render_template('games/post_season.html')
-    first_game = Game.query.filter_by(week=week_num).order_by(Game.start_timestamp).first()
+        return render_template('games/post_season.html', year=year)
+    first_game = Game.query.filter_by(season=year, week=week_num).order_by(Game.start_timestamp).first()
+    if not first_game:
+        flash('No games found for this week.', 'warning')
+        return redirect(url_for('.main', year=year))
+
     lock = dt.datetime.now(dt.timezone.utc).replace(tzinfo=None) > first_game.start_timestamp
     if lock:
-        return post_lock(week_num)
+        return post_lock(year, week_num)
     else:
-        return pre_lock(week_num)
+        return pre_lock(year, week_num)
