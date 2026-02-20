778ed43f1c6d2518127148e9a0072be3551caf71
Ryan Scott <ryan.t.scott73@gmail.com>
2026-02-20 11:07:39 -0700

    Revert "Add static brackets and navigation"
    This reverts commit ccd834b2ca687c5d92448540c7a095853a9b08ac.

--- ufa_picks/playoff/views.py
+++ ufa_picks/playoff/views.py
@@ -6,7 +6,6 @@ from flask import Blueprint, render_template
 from flask_login import login_required
 
 from ufa_picks.playoff.models import PlayoffTeam
-from ufa_picks.user.models import User
 
 blueprint = Blueprint("playoff", __name__, url_prefix="/playoffs", static_folder="../static")
 
@@ -15,13 +14,5 @@ blueprint = Blueprint("playoff", __name__, url_prefix="/playoffs", static_folder
 @blueprint.route("/")
 @login_required
 def main():
-    """Playoff leaderboard"""
-    players = User.query.all()
-    return render_template('playoff/playoffs.html', players=players)
-
-
-@blueprint.route('/bracket-<int:user_id>')
-@login_required
-def bracket(user_id):
-    user = User.get_by_id(user_id)
-    return render_template('playoff/static_bracket.html', player=user)
+    """List weeks."""
+    return render_template('playoff/playoffs.html')
